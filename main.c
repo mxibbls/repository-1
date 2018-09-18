d2f2f54710acdf52794c29a7d04c1cd0e924f95a
mxibbls <maxibbels55@googlemail.com>
2018-09-18 14:45:13 +0200

    erste version

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main (void){
 
     int zahleingabe;
     printf("Bitte geben Sie eine Zahl ein: \n");
-    scanf("%d", &zahleingabe)
+    scanf("%d", &zahleingabe);
      printf("Die eingegebene Zahl ist: %d", zahleingabe);
      return 0;
 }

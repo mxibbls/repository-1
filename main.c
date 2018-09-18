843411e1816c68dc648feed4b8bd357b79fbdb68
mxibbls <maxibbels55@googlemail.com>
2018-09-18 14:37:45 +0200

    erster commit, test

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void){
+
+    int zahleingabe;
+    printf("Bitte geben Sie eine Zahl ein: \n");
+    scanf("%d", &zahleingabe)
+     printf("Die eingegebene Zahl ist: %d", zahleingabe);
+     return 0;
+}
